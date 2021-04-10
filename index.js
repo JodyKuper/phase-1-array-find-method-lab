286b6c7fa346060f3922edc2bd757f39ebdfbb4a
Joel Cooper <jodycooper16@gmail.com>
2021-04-09 17:13:41 -0700

    commit tree b76355f05dcf11cafdbf8337d6819511fe0924e0

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const superbowlWin = (record) =>{
+    
+    const winning=  record.find(element =>element.result === 'W'  )
+    console.log(winning)
+    if (winning){
+        return winning.year
+    }
+    else {
+        return undefined
+    }
+   
+    
+} 
 
+  
